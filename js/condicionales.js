342be2e4150f9cf57aecc37bd768e5fbb3689099
adisonsud <adison.chire.broncano@gmail.com>
2022-08-09 18:18:20 -0500

    Condicion Or - And

--- js/condicionales.js
+++ js/condicionales.js
@@ -1,12 +1,11 @@
+//---------------------------------------------
 // Comparación y Operador Estricto
-
-const numero1 = 20
-const numero2 = "20"
-
 /*
     ==  ( Comparación pero no es estricto)
     === (Comparación estricta - Revisa valor y tipo de dato)
 */
+const numero1 = 20
+const numero2 = "20"
 
 if(numero1 === numero2 ) {
     console.log('Si!, son iguales')
@@ -14,8 +13,22 @@ if(numero1 === numero2 ) {
     console.log('No, no son iguales')
 }
 
-
 const autenticado = true
 if(autenticado) {
     console.log('Si esta autenticado')
+}
+
+//--------------------------------------------
+// Condicion Or - And
+/*
+    || - Al menos una debe cumplirse
+    && - Todas deben cumplirse
+*/
+const saldo = 600
+const pagar = 500
+const tarjeta = true
+if(saldo > pagar && tarjeta ) {
+    console.log('puedes pagar')
+}  else {
+    console.log('No, no puedes pagar')
 }
