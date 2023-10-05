eb7d32a4f9d68abdda91eb6c567f4eac76e5322b
Adison Chire <adison.chire.broncano@gmail.com>
2023-10-05 09:13:46 -0500

    Explica la variable let aplicando typescript

---
+++ dist/variables.js
@@ -0,0 +1,21 @@
+"use strict";
+/*NOTA:
+algunas buenas prácticas de TypeScript:
+1. Nombres Descriptivos: Las variables se nombran de forma descriptiva para indicar su propósito o contenido.
+2. CamelCase: Los nombres de las variables están en camelCase, que es una convención común en TypeScript.
+3. Tipado Explícito: Se han añadido tipos explícitos para las variables para mejorar la legibilidad y la comprensión del código.
+4. Unión de Tipos con number | undefined: Para la variable costo, se ha utilizado el tipo number | undefined para indicar que puede ser undefined inicialmente. Esto proporciona una información clara sobre el posible estado de la variable.
+*/
+//variable -> let
+let clienteActivo = true;
+let precioDescuentazo = 20;
+//let -> permite reasignar a diferencia de const
+clienteActivo = false;
+//Pueden iniciar sin valor
+let costo;
+costo = 100;
+console.log('Cliente Activo: ', clienteActivo);
+console.log('Precio Descuento: ', precioDescuentazo);
+console.log('costo: ', costo);
+/*---------------------------------------------------------*/
+//variable -> const - el mas usado
